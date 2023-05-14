8b0c7785d739364715e4a1f132a37ca9592b2098
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2023-05-14 12:16:27 -0700

    ch06

---
+++ ch06/web_app/README.md
@@ -0,0 +1,30 @@
+# web_app
+
+```sh
+cargo run config.yml
+```
+
+## API
+
+Get item list
+
+```javascript
+GET http://localhost:8080/v1/item/get HTTP/1.1
+```
+
+Create an item
+
+```javascript
+POST http://localhost:8080/v1/item/create/washing HTTP/1.1
+```
+
+Edit an item
+
+```javascript
+POST http://localhost:8080/v1/item/edit HTTP/1.1
+
+{
+    "title": "washing",
+    "status": "DONE"
+}
+```
---
+++ ch06/web_app/src/views/to_do/create.rs
@@ -0,0 +1,30 @@
+use crate::diesel;
+use diesel::prelude::*;
+
+use actix_web::{HttpRequest, HttpResponse};
+
+use crate::json_serialization::to_do_items::ToDoItems;
+
+use crate::database::DB;
+use crate::models::item::item::Item;
+use crate::models::item::new_item::NewItem;
+use crate::schema::to_do;
+
+pub async fn create(req: HttpRequest, db: DB) -> HttpResponse {
+    let title: String = req.match_info().get("title").unwrap().to_string();
+
+    let items = to_do::table
+        .filter(to_do::columns::title.eq(&title.as_str()))
+        .order(to_do::columns::id.asc())
+        .load::<Item>(&db.connection)
+        .unwrap();
+
+    if items.len() == 0 {
+        let new_post = NewItem::new(title);
+        let _ = diesel::insert_into(to_do::table)
+            .values(&new_post)
+            .execute(&db.connection);
+    }
+
+    return HttpResponse::Ok().json(ToDoItems::get_state());
+}
