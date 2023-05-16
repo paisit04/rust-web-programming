11d50abb03619b99870c58510eb1444f5deebd40
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2023-05-15 22:26:33 -0700

    ch09

---
+++ ch09/web_app/src/to_do/structs/done.rs
@@ -0,0 +1,32 @@
+use super::super::enums::TaskStatus;
+use super::base::Base;
+
+pub struct Done {
+    pub super_struct: Base,
+}
+
+impl Done {
+    pub fn new(input_title: &str) -> Self {
+        let base = Base {
+            title: input_title.to_string(),
+            status: TaskStatus::DONE,
+        };
+        return Done { super_struct: base };
+    }
+}
+
+#[cfg(test)]
+mod done_tests {
+    use super::Done;
+
+    use super::TaskStatus;
+    #[test]
+    fn new() {
+        let new_base_struct = Done::new("test title");
+        assert_eq!(
+            String::from("test title"),
+            new_base_struct.super_struct.title
+        );
+        assert_eq!(TaskStatus::DONE, new_base_struct.super_struct.status);
+    }
+}
---
+++ ch09/web_app/src/views/to_do/create.rs
@@ -0,0 +1,31 @@
+use crate::diesel;
+use diesel::prelude::*;
+
+use actix_web::{HttpRequest, HttpResponse};
+
+use crate::json_serialization::to_do_items::ToDoItems;
+
+use crate::database::DB;
+use crate::jwt::JwToken;
+use crate::models::item::item::Item;
+use crate::models::item::new_item::NewItem;
+use crate::schema::to_do;
+
+pub async fn create(token: JwToken, req: HttpRequest, db: DB) -> HttpResponse {
+    let title: String = req.match_info().get("title").unwrap().to_string();
+
+    let items = to_do::table
+        .filter(to_do::columns::title.eq(&title.as_str()))
+        .order(to_do::columns::id.asc())
+        .load::<Item>(&db.connection)
+        .unwrap();
+
+    if items.len() == 0 {
+        let new_post = NewItem::new(title, token.user_id);
+        let _ = diesel::insert_into(to_do::table)
+            .values(&new_post)
+            .execute(&db.connection);
+    }
+
+    return HttpResponse::Ok().json(ToDoItems::get_state(token.user_id));
+}
