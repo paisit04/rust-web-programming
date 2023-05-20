13ef52761b68461d2090db56bfb4762dbd58e114
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2023-05-20 00:05:29 -0700

    ch14

---
+++ ch14/exploring_tokio/examples/with_channel.rs
@@ -0,0 +1,48 @@
+use tokio::sync::mpsc;
+
+#[derive(Debug, Clone)]
+pub enum Order {
+    BUY,
+    SELL,
+}
+#[derive(Debug, Clone)]
+pub struct Message {
+    pub order: Order,
+    pub ticker: String,
+    pub amount: f32,
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel::<Message>(300);
+    let orders = [
+        Message {
+            order: Order::BUY,
+            amount: 5.5,
+            ticker: "BYND".to_owned(),
+        },
+        Message {
+            order: Order::BUY,
+            amount: 5.5,
+            ticker: "NET".to_owned(),
+        },
+        Message {
+            order: Order::BUY,
+            amount: 5.5,
+            ticker: "PLTR".to_owned(),
+        },
+    ];
+
+    tokio::spawn(async move {
+        for order in orders {
+            if let Err(e) = tx.send(order.clone()).await {
+                println!("send error: {:?}", e);
+                return;
+            }
+            println!("sent: {:?}", order);
+        }
+    });
+    while let Some(i) = rx.recv().await {
+        println!("got: {:?}", i);
+    }
+}
